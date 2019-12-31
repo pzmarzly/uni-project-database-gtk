ae3fbfd8db93c61f87299d20dc2e345b771ba4e9
pzmarzly <pawo2500@gmail.com>
2019-12-31 15:09:24 +0100

    split main()

--- main.c
+++ main.c
@@ -6,34 +6,50 @@
 #include "Welcome.h"
 #include "Editor.h"
 
+// No CLI options provided.
+int nothing() {
+    Welcome* rs = welcome_new();
+    welcome_set_quit_on_destroy(rs, true);
+    if (welcome_run(rs))
+        gtk_main();
+    return 0;
+}
+
+// A flag was specified.
+int flags(char *argv[]) {
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        printf("WeźMnie - system rezerwacji sprzętu\n");
+        printf("Użycie: %s [-v|--version|-h|--help|FILE]\n", argv[0]);
+        return 0;
+    }
+    if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) {
+        printf("1.0.0\n");
+        return 0;
+    }
+    printf("Nieznana opcja: %s\n", argv[1]);
+    printf("Wpisz --help, aby uzyskać pomoc.\n");
+    return 1;
+}
+
+// Something else was provided - treat it as a file path.
+int file(char *argv[]) {
+    // Check if file exists.
+    FILE *f = fopen(argv[1], "rb");
+    bool overwrite = f == NULL;
+    if (f != NULL) fclose(f);
+    // Open the editor directly.
+    Editor* re = editor_new(argv[1], overwrite);
+    editor_set_quit_on_destroy(re, true);
+    if (editor_run(re))
+        gtk_main();
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
-
-    if (argc == 1) {
-        Welcome* rs = welcome_new();
-        welcome_set_quit_on_destroy(rs, true);
-        if (welcome_run(rs))
-            gtk_main();
-    } else if (argv[1][0] == '-') {
-        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-            printf("WeźMnie - system rezerwacji sprzętu\n");
-            printf("Użycie: %s [-v|--version|-h|--help|FILE]\n", argv[0]);
-        } else if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-v") == 0) {
-            printf("1.0.0\n");
-        } else {
-            printf("Nieznana opcja: %s\n", argv[1]);
-            printf("Wpisz --help, aby uzyskać pomoc.\n");
-            return 1;
-        }
-    } else {
-        FILE *f = fopen(argv[1], "rb");
-        bool overwrite = f == NULL;
-        if (f != NULL) fclose(f);
-        Editor* re = editor_new(argv[1], overwrite);
-        editor_set_quit_on_destroy(re, true);
-        if (editor_run(re))
-            gtk_main();
-    }
-
-    return 0;
+    if (argc == 1)
+        return nothing();
+    if (argv[1][0] == '-')
+        return flags(argv);
+    return file(argv);
 }
