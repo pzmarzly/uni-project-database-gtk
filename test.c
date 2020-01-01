3413c7fa73f46ffd050fc79fa2dabc67d9c9fd00
pzmarzly <pawo2500@gmail.com>
2020-01-01 01:47:42 +0100

    test handles_data_large

--- test.c
+++ test.c
@@ -11,23 +11,24 @@ void clean() {
 
 void opens_and_overwrites() {
     clean();
-    Repo *r = repo_open("./test-repo.db", true, 1);
+    Repo *r = repo_open("./test-repo.db", true);
+    repo_set_semester_start(r, 1);
     assert(r != NULL);
     assert(repo_get_semester_start(r) == 1);
     repo_close(r);
-    r = repo_open("./test-repo.db", false, 2);
+    r = repo_open("./test-repo.db", false);
     assert(r != NULL);
     assert(repo_get_semester_start(r) == 1);
     repo_close(r);
-    r = repo_open("./test-repo.db", true, 3);
+    r = repo_open("./test-repo.db", true);
     assert(r != NULL);
-    assert(repo_get_semester_start(r) == 3);
+    assert(repo_get_semester_start(r) != 1);
     repo_close(r);
 }
 
 void handles_data() {
     clean();
-    Repo *r = repo_open("./test-repo.db", true, 1);
+    Repo *r = repo_open("./test-repo.db", true);
     Equipment eq = {1, {0}, 3};
     repo_set(r, TableEquipment, 0, &eq);
     StringMetadata str = {3, 5};
@@ -42,7 +43,7 @@ void handles_data() {
     assert(str.len == 5);
     repo_close(r);
 
-    r = repo_open("./test-repo.db", false, 1);
+    r = repo_open("./test-repo.db", false);
     assert(repo_len(r, TableStringMetadata) == 1);
     str.len = 1;
     assert(repo_get(r, TableStringMetadata, 0, &str) == true);
@@ -61,7 +62,7 @@ void handles_data() {
 
 void handles_strings() {
     clean();
-    Repo *r = repo_open("./test-repo.db", true, 1);
+    Repo *r = repo_open("./test-repo.db", true);
     char *c1 = "Troche tekstu.", *c2 = "Drugi tekst.", *c3 = "Trzeci tekst.";
     char *str = c1;
 
@@ -99,10 +100,61 @@ void handles_strings() {
     repo_close(r);
 }
 
+void handles_data_large() {
+    clean();
+    Repo *r = repo_open("./test-repo.db", true);
+    Equipment eq = {1, {0}, 3};
+
+    for (ID i = 0; i < 1000; i++) {
+        eq.description = i;
+        repo_set(r, TableEquipment, i, &eq);
+    }
+    assert(repo_len(r, TableEquipment) == 1000);
+
+    // Delete every second one.
+    for (ID i = 0, deleted = 0; i < 1000; i+= 2, deleted++) {
+        repo_del(r, TableEquipment, i - deleted);
+    }
+    assert(repo_len(r, TableEquipment) == 500);
+
+    for (ID i = 0; i < 500; i++) {
+        assert(repo_get(r, TableEquipment, i, &eq) == true);
+        assert(eq.description == 2 * i + 1);
+    }
+
+    assert(repo_len(r, TableEquipment) == 500);
+    repo_close(r);
+    r = repo_open("./test-repo.db", false);
+    assert(repo_len(r, TableEquipment) == 500);
+
+    for (ID i = 0; i < 500; i++) {
+        repo_get(r, TableEquipment, i, &eq);
+        assert(eq.description == 2 * i + 1);
+    }
+    assert(repo_len(r, TableEquipment) == 500);
+
+    repo_del_n(r, TableEquipment, 100, 100);
+    assert(repo_len(r, TableEquipment) == 400);
+    repo_close(r);
+    r = repo_open("./test-repo.db", false);
+    assert(repo_len(r, TableEquipment) == 400);
+    assert(repo_get(r, TableEquipment, 0, &eq) == true);
+    assert(eq.description == 1);
+    assert(repo_get(r, TableEquipment, 99, &eq) == true);
+    assert(eq.description == 2 * 99 + 1);
+    assert(repo_get(r, TableEquipment, 100, &eq) == true);
+    assert(eq.description == 2 * 200 + 1);
+    assert(repo_get(r, TableEquipment, 101, &eq) == true);
+    assert(eq.description == 2 * 201 + 1);
+    repo_close(r);
+}
+
 int main() {
     opens_and_overwrites();
     handles_data();
     handles_strings();
+
+    handles_data_large();
 
     printf("OK\n");
     clean();
