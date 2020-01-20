ed7c35bfa2bcd4146768f12eb5a7d7e9aa4cb1f0
pzmarzly <pawo2500@gmail.com>
2020-01-20 17:25:10 +0100

    fmt

--- RepoLogic.c
+++ RepoLogic.c
@@ -1,17 +1,24 @@
 #include "RepoLogic.h"
 #include "RepoData.h"
 
-bool periodic_slot_conflict(Repo *repo, PeriodicReservation *res, ID res2_id, ID eq_id) {
+bool periodic_slot_conflict(Repo *repo, PeriodicReservation *res, ID res2_id,
+                            ID eq_id) {
   PeriodicReservation r;
   repo_get(repo, TablePeriodicReservation, res2_id, &r);
   PeriodicReservation *res2 = &r;
 
-  if (res2->item != eq_id) return false;
-  if (res2->active_until <= res->active_since) return false;
-  if (res->active_until <= res2->active_since) return false;
-  if (res2->day != res->day) return false;
-  if (res->end <= res2->start) return false;
-  if (res2->end <= res->start) return false;
+  if (res2->item != eq_id)
+    return false;
+  if (res2->active_until <= res->active_since)
+    return false;
+  if (res->active_until <= res2->active_since)
+    return false;
+  if (res2->day != res->day)
+    return false;
+  if (res->end <= res2->start)
+    return false;
+  if (res2->end <= res->start)
+    return false;
   return true;
 }
 
@@ -19,8 +26,10 @@ bool available_periodic_slot(Repo *repo, PeriodicReservation *res, ID res_id,
                              ID eq_id) {
   ID max = repo_len(repo, TablePeriodicReservation);
   for (ID i = 0; i < max; i++) {
-    if (i == res_id) continue;
-    if (periodic_slot_conflict(repo, res, i, eq_id)) return false;
+    if (i == res_id)
+      continue;
+    if (periodic_slot_conflict(repo, res, i, eq_id))
+      return false;
   }
   return true;
 }
