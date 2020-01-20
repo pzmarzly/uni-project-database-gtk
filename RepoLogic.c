70ba49f5fd1a8b25a6353ea5ba4afc819e6fabc2
pzmarzly <pawo2500@gmail.com>
2020-01-20 17:59:09 +0100

    refactor RepoLogic

--- RepoLogic.c
+++ RepoLogic.c
@@ -1,23 +1,41 @@
 #include "RepoLogic.h"
 #include "RepoData.h"
 
-bool periodic_slot_conflict(Repo *repo, PeriodicReservation *res, ID res2_id,
-                            ID eq_id) {
-  PeriodicReservation r;
-  repo_get(repo, TablePeriodicReservation, res2_id, &r);
-  PeriodicReservation *res2 = &r;
+bool periodic_is_active_within_time_period(PeriodicReservation *res,
+                                        Timestamp start, Timestamp end) {
+  if (res->active_until <= start)
+    return false;
+  if (res->active_since >= end)
+    return false;
+  return true;
+}
 
-  if (res2->item != eq_id)
+OneTimeReservation *reservations_for_time_period(Repo *repo, Timestamp start,
+                                                 Timestamp end) {
+  ID max = repo_len(repo, TablePeriodicReservation);
+  for (ID i = 0; i < max; i++) {
+    PeriodicReservation res;
+    repo_get(repo, TablePeriodicReservation, i, &res);
+    if (periodic_is_active_within_time_period(&res, start, end)) {
+      // TODO: generate OneTimeReservation
+    }
+  }
+  return NULL;
+}
+
+bool periodic_are_conflicting(PeriodicReservation *res1,
+                            ID eq_res1_candidate, PeriodicReservation *res2) {
+  if (res2->item != eq_res1_candidate)
     return false;
-  if (res2->active_until <= res->active_since)
+  if (res2->active_until <= res1->active_since)
     return false;
-  if (res->active_until <= res2->active_since)
+  if (res1->active_until <= res2->active_since)
     return false;
-  if (res2->day != res->day)
+  if (res2->day != res1->day)
     return false;
-  if (res->end <= res2->start)
+  if (res1->end <= res2->start)
     return false;
-  if (res2->end <= res->start)
+  if (res2->end <= res1->start)
     return false;
   return true;
 }
@@ -28,7 +46,9 @@ bool available_periodic_slot(Repo *repo, PeriodicReservation *res, ID res_id,
   for (ID i = 0; i < max; i++) {
     if (i == res_id)
       continue;
-    if (periodic_slot_conflict(repo, res, i, eq_id))
+    PeriodicReservation res2;
+    repo_get(repo, TablePeriodicReservation, i, &res2);
+    if (periodic_are_conflicting(res, eq_id, &res2))
       return false;
   }
   return true;
