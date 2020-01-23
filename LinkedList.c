cd8888c4d663412b48603df52ccd271d9c17f78e
pzmarzly <pawo2500@gmail.com>
2020-01-23 01:08:41 +0100

    fmt

--- LinkedList.c
+++ LinkedList.c
@@ -3,52 +3,52 @@
 #include <string.h>
 
 typedef struct Node {
-    void *data;
-    struct Node *next;
+  void *data;
+  struct Node *next;
 } Node;
 
 struct LinkedList {
-    Node *start;
-    Node *end;
-    int len;
+  Node *start;
+  Node *end;
+  int len;
 };
 
-LinkedList* linked_list_new() {
-    LinkedList *ret = malloc(sizeof(LinkedList));
-    ret->start = NULL;
-    ret->end = NULL;
-    ret->len = 0;
-    return ret;
+LinkedList *linked_list_new() {
+  LinkedList *ret = malloc(sizeof(LinkedList));
+  ret->start = NULL;
+  ret->end = NULL;
+  ret->len = 0;
+  return ret;
 }
 
 void linked_list_add(LinkedList *list, void *item) {
-    Node* node = malloc(sizeof(Node));
-    node->data = item;
-    node->next = NULL;
-    if (list->len == 0) {
-        list->start = node;
-        list->end = node;
-    } else {
-        list->end->next = node;
-        list->end = node;
-    }
-    list->len++;
+  Node *node = malloc(sizeof(Node));
+  node->data = item;
+  node->next = NULL;
+  if (list->len == 0) {
+    list->start = node;
+    list->end = node;
+  } else {
+    list->end->next = node;
+    list->end = node;
+  }
+  list->len++;
 }
 
 int linked_list_into_array(LinkedList *list, int element_size, void **dest) {
-    void *arr = malloc(list->len * element_size);
-    int i = 0;
-    Node *node = list->start;
-    while (node != NULL) {
-        memcpy(arr + i * element_size, node->data, element_size);
-        i++;
+  void *arr = malloc(list->len * element_size);
+  int i = 0;
+  Node *node = list->start;
+  while (node != NULL) {
+    memcpy(arr + i * element_size, node->data, element_size);
+    i++;
 
-        Node *old = node;
-        node = node->next;
-        free(old);
-    }
-    free(list);
+    Node *old = node;
+    node = node->next;
+    free(old);
+  }
+  free(list);
 
-    *dest = arr;
-    return i;
+  *dest = arr;
+  return i;
 }
--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,6 @@
 
 typedef struct LinkedList LinkedList;
 
-LinkedList* linked_list_new();
+LinkedList *linked_list_new();
 void linked_list_add(LinkedList *list, void *item);
 int linked_list_into_array(LinkedList *list, int element_size, void **dest);
